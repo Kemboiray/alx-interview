634bd508e2b375d033d3712d6b5f327e0e2b0702
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-09-29 06:02:09 +0300

    see diff

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,11 @@
-# In python code, this can be written as:
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing 
+    the Pascal’s triangle of n
+    """
     if n == 0:
         return []
     elif n == 1:
