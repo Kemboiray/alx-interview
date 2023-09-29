25e27b93d4bb89f49365fd591ecc72e40de7967d
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-09-29 13:23:40 +0300

    Rewrite `pascal_triangle()` iteratively

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""Pascal's Triangle"""
+# -*- coding: utf-8 -*-
+"""This module defines a function that creates a Pascal triangle."""
 
 
-def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing
-    the Pascal’s triangle of n
-    """
-    if n == 0:
-        return []
-    elif n == 1:
-        return [[1]]
-    else:
-        new_row = [1]
-        result = pascal_triangle(n-1)
-        last_row = result[-1]
-        for i in range(len(last_row)-1):
-            new_row.append(last_row[i] + last_row[i+1])
-        new_row += [1]
-        result.append(new_row)
-        return result
+def pascal_triangle(n: int) -> list:
+    """Return a pascal triangle"""
+    triangle = [[] for _ in range(n)]
+    i = 0
+    while i < n:
+        j = 0
+        while j <= i:
+            if j == 0 or j == i:
+                triangle[i].append(1)
+            else:
+                triangle[i].append(triangle[i-1][j-1] + triangle[i-1][j])
+            j += 1
+        i += 1
+    return triangle
