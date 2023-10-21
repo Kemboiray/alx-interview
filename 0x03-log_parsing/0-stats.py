c2cd8d02678c8b1c8582e54c2c24e7dfc1a8f8b9
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-22 01:54:54 +0300

    Please the checker

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -75,11 +75,10 @@ def main() -> None:
             line_no += 1
             if line_no % 10 == 0:
                 print_stats(file_size, codes_dict)
-        if line_no % 10:
+        if line_no % 10 or line_no == 0:
             print_stats(file_size, codes_dict)
     except KeyboardInterrupt:
         print_stats(file_size, codes_dict)
-        # exit(1)
 
 
 if __name__ == "__main__":
