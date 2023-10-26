8aeac5ecf09e8532593549d8f034d69e11b52a6d
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-26 23:44:15 +0300

    Use f strings

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,7 @@ def validUTF8(data):
     """Validate UTF-8 encoding for a data stream"""
     count = 0
     for num in data:
-        bin_rep = format(
-            num, '#010b')[-8:]  # Get the binary representation of the number
+        bin_rep = f"{num:08b}"  # Get the binary representation of the number
         if count == 0:  # If this is the start of a new character
             for bit in bin_rep:
                 if bit == '0':
