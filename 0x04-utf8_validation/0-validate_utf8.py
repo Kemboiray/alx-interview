2b1fea2a1921e58a222e33e18e7c731a8f3e4d65
sandbox_user <user@sandbox.com>
2023-10-26 21:30:25 +0000

    Rewrite unsupported method

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,7 @@
 
 def validUTF8(data):
     """Validate UTF-8 encoding for a data stream"""
-    bin_reps = [format(d, '#010b').removeprefix('0b') for d in data]
+    bin_reps = [format(d, '#010b')[2:] for d in data]
     return all([
         len(b) == 8 and (b.startswith('0') or b.startswith('110')
                          or b.startswith('1110') or b.startswith('1111'))
