686b621c46c3e2dc75beed214a8ea7f503d7a944
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-27 00:46:52 +0300

    see diff

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,26 @@
 
 def validUTF8(data):
     """Validate UTF-8 encoding for a data stream"""
-    count = 0
-    for num in data:
-        bin_rep = format(
-            num, '#010b')[-8:]  # Get the binary representation of the number
-        if count == 0:  # If this is the start of a new character
-            for bit in bin_rep:
-                if bit == '0':
-                    break
-                count += 1
-            if count == 0:
-                continue
-            if count == 1 or count > 4:
-                return False
-        else:
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
-                return False
-        count -= 1
-    return count == 0
+    try:
+        bytes(data).decode('utf-8')
+        return True
+    except (ValueError, UnicodeDecodeError, TypeError):
+        return False
+    # count = 0
+    # for num in data:
+    #     bin_rep = format(
+    #         num, '#010b')[-8:]  # Get the binary representation of the number
+    #     if count == 0:  # If this is the start of a new character
+    #         for bit in bin_rep:
+    #             if bit == '0':
+    #                 break
+    #             count += 1
+    #         if count == 0:
+    #             continue
+    #         if count == 1 or count > 4:
+    #             return False
+    #     else:
+    #         if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+    #             return False
+    #     count -= 1
+    # return count == 0
