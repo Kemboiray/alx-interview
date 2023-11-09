975c967a9af50e7fac9daf0d1d494b3815b30002
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-10 01:41:29 +0300

    Refactor?

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,30 @@
 #!/usr/bin/node
 // Script that prints all characters of a Star Wars movie
 // The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+// Display one character name by line in the SAME ORDER of the list “characters” in the /films/ response
+// You must use the Star wars API
+// You must use the module request
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+function getCharacters (characters) {
+  request(characters, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+    }
+  });
+}
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+    for (const character of characters) {
+      getCharacters(character);
     }
   }
 });
