c053cff4dc0dd7c6571ca13a62c2f4944d227272
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-16 11:38:43 +0000

    Simplify

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,7 +12,7 @@ Given an n x n 2D matrix, rotate it 90 degrees clockwise.
 def rotate_2d_matrix(matrix):
     """Rotate a 2D matrix 90 degrees clockwise in place."""
     size = len(matrix)
-    copy = [[matrix[row][col] for col in range(size)] for row in range(size)]
+    copy = [row[:] for row in matrix]
     for row in range(size):
         for col in range(size - 1, -1, -1):
             matrix[row][-1 - col] = copy[col][row]
